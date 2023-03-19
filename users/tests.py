08a224e4a58f5ee20a27bde23d1558b72dc4268c
oybek.john <oybek.john01@gmail.com>
2023-03-19 20:48:54 +0500

    changes testCase

--- users/tests.py
+++ users/tests.py
@@ -7,26 +7,28 @@ from django.urls import reverse
 
 class RegisterTestCase(TestCase):
     def test_unique_username(self):
-        response1 = self.client.post(
-            reverse('register'),
-            data = {
-                'username':'oybek',
-                'first_name':'Oybek',
-                'last_name': 'Saydullayev',
-                'email': 'oybekjohn01@gmail.com',
-                'password': 'oybek0121'
-            }
+        user=User.objects.create(
+            username='oybek',
+            first_name='Oybek',
+            last_name='Saydullayev',
         )
+        user.set_password(
+            'anypassword'
+        )
+        user.save()
+
         response2 = self.client.post(
             reverse('register'),
-            data = {
+            data={
                 'username':'oybek',
                 'first_name':'Oybeke',
                 'last_name': 'Saydullayeve',
                 'email': 'oybekjohn01@gmeail.com',
-                'password': 'oybek01e21'
+                'password': 'oybek01e21',
             }
         )
+        user_count = User.objects.count()
+        self.assertEqual(user_count, 1)
 
         self.assertFormError(response2, 'user_form', 'username', 'A user with that username already exists.')
 
@@ -43,3 +45,16 @@ class RegisterTestCase(TestCase):
         )
         count = User.objects.count()
         self.assertEqual(count, 0)
+
+    def test_valid_username(self):
+        self.client.post(
+            reverse('register'),
+            data={
+                'first_name': 'Oybek',
+                'last_name': 'Saydullayev',
+                'email': 'oybekjohn01gmail.com',
+                'password': 'oybek0121'
+            }
+        )
+        user_count = User.objects.count()
+        self.assertEqual(user_count, 0)
