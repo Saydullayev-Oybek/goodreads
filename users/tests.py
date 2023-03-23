e3857ef854d8dea8604500c8d5d408380a3aa388
oybek.john <oybek.john01@gmail.com>
2023-03-23 18:14:29 +0500

    Login Test

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user
 from django.contrib.auth.models import User
 from django.test import TestCase
 
@@ -20,8 +21,8 @@ class RegisterTestCase(TestCase):
         response2 = self.client.post(
             reverse('register'),
             data={
-                'username':'oybek',
-                'first_name':'Oybeke',
+                'username': 'oybek',
+                'first_name': 'Oybeke',
                 'last_name': 'Saydullayeve',
                 'email': 'oybekjohn01@gmeail.com',
                 'password': 'oybek01e21',
@@ -52,9 +53,63 @@ class RegisterTestCase(TestCase):
             data={
                 'first_name': 'Oybek',
                 'last_name': 'Saydullayev',
-                'email': 'oybekjohn01gmail.com',
+                'email': 'oybekjohn01@gmail.com',
                 'password': 'oybek0121'
             }
         )
         user_count = User.objects.count()
         self.assertEqual(user_count, 0)
+
+
+class LoginTestCase(TestCase):
+    def test_success_login(self):
+        db_user = User.objects.create(
+            username='oybek',
+            first_name='Oybek',
+            last_name='saydullayev',
+            email='oybek@gmail.com'
+        )
+        db_user.set_password('password')
+        db_user.save()
+
+        self.client.post(
+            reverse('login'),
+            data={
+                'username': 'oybek',
+                'password': 'password'
+            }
+        )
+        user = get_user(self.client)
+        self.assertTrue(user.is_authenticated)
+
+    def test_wrong_credentials(self):
+        db_user = User.objects.create(
+            username='oybek',
+            first_name='Oybek',
+            last_name='saydullayev',
+            email='oybek@gmail.com'
+        )
+        db_user.set_password('password')
+        db_user.save()
+
+        self.client.post(
+            reverse('login'),
+            data={
+                'username': 'oybek',
+                'password': 'wrong-password'
+            }
+        )
+        user = get_user(self.client)
+        self.assertFalse(user.is_authenticated)
+
+
+        self.client.post(
+            reverse('login'),
+            data={
+                'username': 'wrong-username',
+                'password': 'password'
+            }
+        )
+
+        user = get_user(self.client)
+        self.assertFalse(user.is_authenticated)
